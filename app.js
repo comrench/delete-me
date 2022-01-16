c0e70fd57e8ba82725b30e3c852e49be343d9369
Mahesh Kumar <maheshkumar@Maheshs-Mac-mini.local>
2022-01-16 00:55:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import routes from './routes/etfRouter.js';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+
+app.use('/api/', routes);
+
+app.server = app.listen(port, () => {
+  console.log(`Running on port: ${port}`);
+});
---
+++ routes/etfRouter.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import axios from 'axios';
+
+const routes = express.Router();
+
+routes.get('/loadETFs', async (req, res, next) => {
+  console.log('routes -> /loadETFs');
+  try {
+    const response = await axios.get(
+      // 'https://jsonplaceholder.typicode.com/todos'
+      'https://purposecloud.s3.amazonaws.com/challenge-data.json'
+    );
+    res.send(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+routes.post('/saveETFs', async (req, res, next) => {
+  console.log('routes -> /saveETFs');
+  try {
+    res.json(req.body);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default routes;
